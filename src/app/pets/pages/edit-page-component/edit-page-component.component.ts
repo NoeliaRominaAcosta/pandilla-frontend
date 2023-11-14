8023b0d0fcd5c8d8c1e19eec10d624676fefb785
NoeliaRominaAcosta <noeliaromina20@gmail.com>
2023-11-14 17:29:02 -0300

    update image management

---
+++ src/app/pets/pages/edit-page-component/edit-page-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-page-component',
+  templateUrl: './edit-page-component.component.html',
+  styles: [
+  ]
+})
+export class EditPageComponentComponent {
+
+}
